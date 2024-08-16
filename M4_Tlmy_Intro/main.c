e709ba80e23cf2d8364c4c52d7a97d9484444b2f
psoligo@unlam.edu.ar <psoligo@unlam.edu.ar>
2024-08-16 09:29:20 -0300

    Mostrar datos en hexa

--- M4_Tlmy_Intro/main.c
+++ M4_Tlmy_Intro/main.c
@@ -4,15 +4,46 @@
 #include <stdint.h>
 #include "TlmyProducer.h"
 
+#define HARDCODE_PKTS_COUNT 3
+
+//Esta funcion no deberia estar aqui
+int showHex(void* ele);
+//Esta funcion no deberia estar aqui
+void* map(void* vec, unsigned nelements, unsigned esize, int action(void* ele));
+
+
 int main()
 {
-    int cont = 0;
-    uint8_t buffer[PKT_SIZE];
 
-    while(getPktTlmy(buffer)){
+    uint8_t buffer[PKT_SIZE];
+    int i;
+
+    for(i=0;i<HARDCODE_PKTS_COUNT;i++){
+        if (getPktTlmy(buffer)){
+            //Muestro solo los 20 primeros
+            map(buffer, /*PKT_SIZE*/ 20, sizeof(uint8_t), showHex);
+            puts("");
+        }
     }
 
-    printf("Total de paquetes: %d", cont);
 
     return 0;
 }
+
+//Esta funcion no deberia estar aqui
+int showHex(void* ele){
+    uint8_t* pele = (uint8_t*)ele;
+    return printf("%04X ", *pele);
+}
+
+//Esta funcion no deberia estar aqui
+void* map(void* vec, unsigned nelements, unsigned esize, int action(void* ele)){
+    int i;
+    void* ini = vec;
+    for(i=0;i<nelements;i++){
+        action(vec);
+        vec++;
+    }
+    return ini;
+}
+
